75286ed048bfd018610d551464289d5aee77fe4c
Amy Maple <amymaple16@gmail.com>
2022-07-17 00:00:13 -0700

    made col and row with bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,18 +16,37 @@
                 </div>
 
  
-                
+                <?php $counter = 0; ?>
+                <?php $closeBrackets= 0; ?>
 
                 @foreach ($sticky_notes as $sticky_note) 
-                    <div class="card border-primary mb-3" style="max-width: 18rem;">
-                        <div class="card-header">{{$sticky_note->title}} </div>
-                        <div class="card-body text-primary">
-                            <h5 class="card-title">Primary card title</h5>
-                            <p class="card-text">{{$sticky_note->description}}</p>
-                        </div>
-                    </div>
-                @endforeach
+                    
+                    @if($counter % 2 == 0  || $counter == 0   )
+                    
+                        <div class="row"  >
+                    @endif
 
+                    
+                    <div class="col"  >
+
+                        <div class="card border-primary mb-3" style="max-width: 18rem;">
+                            <div class="card-header">{{$sticky_note->title}} </div>
+                            <div class="card-body text-primary">
+                                <h5 class="card-title">Primary card title</h5>
+                                <p class="card-text">{{$sticky_note->description}}</p>
+                            </div>
+                        </div>
+
+                    </div >
+                    
+                    
+                    @if($closeBrackets )
+                        </div >
+                    @endif
+
+                    <?php $closeBrackets= 1; ?>
+                    <?php $counter++; ?>
+                @endforeach
 
 
 
